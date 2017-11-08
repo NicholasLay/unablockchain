396555a3ae227b07ee9681f9759efc44997b1bf1
ariadan <ariadan@id.ibm.com>
2017-11-08 13:34:07 +0700

    Added feature for Authentication in API

--- src/main/java/com/ibm/bpm/adira/LoginController.java
+++ src/main/java/com/ibm/bpm/adira/LoginController.java
@@ -6,18 +6,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class LoginController 
 {
 	@PostMapping("/api/login")
-	public ResponseEntity<?> login(@RequestBody Login login)
+	public ResponseEntity<?> login(@RequestHeader(value="username") String username,
+			@RequestHeader(value="password") String password,
+			@RequestBody Login login)
 	{
-		final String uri = "http://www.nba.com";
-		RestTemplate restTemplate = new RestTemplate();
-		String result = restTemplate.getForObject(uri, String.class);
-		return new ResponseEntity("Successfully Login"+result, new HttpHeaders(),HttpStatus.OK);
+		//final String uri = "http://www.nba.com";
+		//RestTemplate restTemplate = new RestTemplate();
+		//String result = restTemplate.getForObject(uri, String.class);
+		Authentication authentication = new Authentication(username,password);
+		String result = authentication.getAuthentication();
+		HttpStatus status;
+		String message = "";
+		if (result == "OK")
+		{
+			status = HttpStatus.OK;
+			message = "Login Success";
+		}
+		else
+		{
+			status = HttpStatus.FORBIDDEN;
+			message = "Login Failed ";
+		}
+		return new ResponseEntity(message+username+"/"+password+result, new HttpHeaders(),status);
 	}
 	
 }
