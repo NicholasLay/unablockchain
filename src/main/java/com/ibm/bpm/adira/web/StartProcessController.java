0284b42f0664190de7c6046d8192a0347a9d207c
ariadan <ariadan@id.ibm.com>
2017-11-15 15:36:12 +0700

    commit new sync controller

--- src/main/java/com/ibm/bpm/adira/web/StartProcessController.java
+++ src/main/java/com/ibm/bpm/adira/web/StartProcessController.java
@@ -1,8 +1,5 @@
 package com.ibm.bpm.adira.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,22 +9,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.ibm.bpm.adira.domain.StartProcessRequestBean;
-import com.ibm.bpm.adira.service.ProcessService;
-import com.ibm.bpm.adira.service.impl.ProcessServiceImpl;
 
 @Controller
 public class StartProcessController 
 {
-private static final Logger logger = LoggerFactory.getLogger(ProcessServiceImpl.class);
-	
-	@Autowired
-    private ProcessService processService;
-	
-	@RequestMapping(value="/startProcess", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> login(@RequestBody StartProcessRequestBean starProcessRequest)
+	@RequestMapping(value="/startProcessIDE", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> login(@RequestBody StartProcessRequestBean startProcess)
 	{
-<<<<<<< HEAD
 		String response = "{\"orderID\":\"" +startProcess.getOrderID()+ "\","+
         		"\"processID\":\"" +startProcess.getProcessID()+ "\","+
         		"\"taskID\":\"" +startProcess.getTaskID()+ "\","+
@@ -37,27 +27,6 @@ private static final Logger logger = LoggerFactory.getLogger(ProcessServiceImpl.
         		"\"status\":\"Complete\""+ 
         		"}";
 		return new ResponseEntity(response, new HttpHeaders(),HttpStatus.OK);
-=======
-		String orderId	= starProcessRequest.getOrderID();
-		int processId	= starProcessRequest.getProcessID();
-		int brmsScoring = starProcessRequest.getBrmsScoring();
-		int taskId 		= starProcessRequest.getTaskID();
-		Boolean mayor 	= starProcessRequest.getMayor();
-		
-		String logTracker = 
-				"From acction: "+ 
-				"Order ID ="+orderId+
-				"Process ID ="+processId+
-				"Task ID = "+taskId+
-				"BRMS ="+brmsScoring+
-				"Mayor ="+mayor;
-		
-		logger.info(logTracker);
-		
-		processService.process(orderId,processId,taskId);
-		
-		
-		return new ResponseEntity("{\"status\": \"Success\"}", new HttpHeaders(),HttpStatus.OK);
->>>>>>> c2a4823890d75341b5afcfce9742b3e18c852e30
 	}
+	
 }
