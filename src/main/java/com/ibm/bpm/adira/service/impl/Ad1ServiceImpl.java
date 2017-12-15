302dbeffc5dbc6509503b24e2e46c2cfff2a3abe
ariadan <ariadan@id.ibm.com>
2017-12-15 11:29:09 +0700

    add logging

--- src/main/java/com/ibm/bpm/adira/service/impl/Ad1ServiceImpl.java
+++ src/main/java/com/ibm/bpm/adira/service/impl/Ad1ServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ibm.bpm.adira.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -8,11 +10,14 @@ import org.springframework.web.client.RestTemplate;
 import com.google.gson.Gson;
 import com.ibm.bpm.adira.domain.Ad1AuthRequest;
 import com.ibm.bpm.adira.service.Ad1Service;
+import com.ibm.bpm.adira.web.StartProcessController;
 
 public class Ad1ServiceImpl implements Ad1Service
 {
 
 	private static final String AD1_URL =  "http://10.50.1.18:99/Portsightdev/api/AuthenticateUser";
+	private static final Logger logger = LoggerFactory.getLogger(Ad1ServiceImpl.class);
+	
 	@Override
 	public String authResponse(String login, String password) 
 	{
@@ -22,11 +27,13 @@ public class Ad1ServiceImpl implements Ad1Service
 		ad1AuthRequest.setLogin(login);
 		ad1AuthRequest.setPassword(password);
 		ad1Request = json.toJson(ad1AuthRequest);
+		logger.info("[Ad1ServiceImpl] Request to Ad1 from API."+ ad1Request);
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<String> entity = new HttpEntity<String>(ad1Request,httpHeaders);
 		RestTemplate restTemplate = new RestTemplate();
 		String response = restTemplate.postForObject(AD1_URL, entity, String.class);
+		logger.info("[Ad1ServiceImpl] Response from Ad1 to API."+ response);
 		return response;
 	}
 
