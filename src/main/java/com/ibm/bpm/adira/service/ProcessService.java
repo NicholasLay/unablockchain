01abe9b1de268a3ceebec50978c3261ad53fffb5
ariadan <ariadan@id.ibm.com>
2018-01-23 17:06:44 +0700

    currentLevelOverride parameter add

--- src/main/java/com/ibm/bpm/adira/service/ProcessService.java
+++ src/main/java/com/ibm/bpm/adira/service/ProcessService.java
@@ -2,5 +2,5 @@ package com.ibm.bpm.adira.service;
 
 public interface ProcessService {
 
-	void processCurrentState(String service,String orderID, int processID, int taskID, Integer maxLevel, String approvalResult);
+	void processCurrentState(String service,String orderID, int processID, int taskID, Integer maxLevel, String approvalResult, Integer currentLevelOverride);
 }
